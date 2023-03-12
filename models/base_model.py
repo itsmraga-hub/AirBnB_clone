fd8399322c31d7dae795b5be3105a92cb9ab5eae
itsmraga-hub <ragawilliam570@gmail.com>
2023-03-12 21:14:54 +0300

    Update base_model importation

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -37,7 +37,7 @@ class BaseModel:
         """updates last update time
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """creates a new dictionary, adding a key and returning
